d1b7966245f4d6e48a051061574efff3c49295ae
kadirnar <kadir.nar@hotmail.com>
2023-11-22 20:52:01 +0300

    feat: ⭐️Now supports all openai-whisper models! 🚀

--- whisperplus/demo.py
+++
@@ -1,9 +0,0 @@
-from whisperplus.pipelines.whisper import SpeechToTextPipeline
-from whisperplus.utils.download_utils import download_and_convert_to_mp3
-
-
-def main(url):
-    video_path = download_and_convert_to_mp3(url)
-    pipeline = SpeechToTextPipeline()
-    transcript = pipeline(audio_path=video_path, model_id="openai/whisper-large-v3", language="turkish")
-    return transcript
