e18a52204eb94e60ae5dd001f1e687fe0ceab35c
Mert Cobanov <mertcobanov@gmail.com>
2023-11-22 17:12:08 -0800

    Update better downloader and audio convertion

--- whisperplus/utils/download_utils.py
+++ whisperplus/utils/download_utils.py
@@ -1,17 +1,48 @@
+import os
+import logging
 from pathlib import Path
+from typing import Optional
 
 from pytube import YouTube
+from moviepy.editor import AudioFileClip
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
-def download_and_convert_to_mp3(url: str, output_path: str = "output", filename: str = "test") -> str:
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
-    out_file = video.download(output_path=output_path, filename=filename)
+def download_and_convert_to_mp3(
+    url: str, output_path: str = "output", filename: str = "test"
+) -> Optional[str]:
+    try:
+        yt = YouTube(url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
 
-    new_file = Path(out_file).with_suffix('.mp3')
+        if audio_stream is None:
+            logging.warning("No audio streams found")
+            return None
 
-    if new_file.exists():
-        new_file.unlink()
+        Path(output_path).mkdir(parents=True, exist_ok=True)
 
-    Path(out_file).rename(new_file)
-    return str(new_file)
+        mp3_file_path = os.path.join(output_path, filename + ".mp3")
+        logging.info(f"Downloading started... {mp3_file_path}")
+
+        downloaded_file_path = audio_stream.download(output_path)
+
+        audio_clip = AudioFileClip(downloaded_file_path)
+        audio_clip.write_audiofile(
+            mp3_file_path, codec="libmp3lame", verbose=False, logger=None
+        )
+        audio_clip.close()
+
+        if Path(downloaded_file_path).suffix != ".mp3":
+            os.remove(downloaded_file_path)
+
+        logging.info(
+            f"Download and conversion successful. File saved at: {mp3_file_path}"
+        )
+        return str(mp3_file_path)
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return None
