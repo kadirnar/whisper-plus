e6b2f5c04e07e99801a9cfacd46bbfef6058aeb3
GuldenizBektas <gdenizbektass@gmail.com>
2023-11-24 11:48:55 +0300

    Change output type to mp3

--- bark/text_to_speech.py
+++ bark/text_to_speech.py
@@ -1,5 +1,7 @@
 import logging
 
+import numpy as np
+from pydub import AudioSegment
 from scipy.io.wavfile import write
 
 from bark import SAMPLE_RATE, generate_audio, preload_models
@@ -7,7 +9,7 @@ from bark import SAMPLE_RATE, generate_audio, preload_models
 
 class TextToSpeechPipeline:
 
-    def __init__(self, input_text: str, output_path: str = "output.wav"):
+    def __init__(self, input_text: str, output_path: str = "output.mp3"):
         self.input_text = input_text
         self.output_path = output_path
 
@@ -21,4 +23,12 @@ class TextToSpeechPipeline:
             return
 
         audio_array = generate_audio(self.input_text)
-        write(self.output_path, rate=SAMPLE_RATE, data=audio_array)
+        # Convert NumPy array to AudioSegment
+        # Convert NumPy array to AudioSegment
+        audio_array = (audio_array * 32767).astype(np.int16)  # type: ignore # Scale to 16-bit integer
+        audio_segment = AudioSegment(
+            audio_array.tobytes(), frame_rate=SAMPLE_RATE, sample_width=2, channels=1)
+
+        audio_segment.export(self.output_path, format="mp3")
+
+        print(f"Speech saved to {self.output_path}")
