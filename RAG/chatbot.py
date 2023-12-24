6c51e0f4bf5b271a0afd4dfa2f1f0ee9826f91de
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2023-12-24 11:42:46 +0530

    Update chatbot.py
    added better log & error handling

--- RAG/chatbot.py
+++ RAG/chatbot.py
@@ -21,55 +21,114 @@ DATABASE_PATH = '/tmp/lancedb'
 class ChatWithVideo:
     @staticmethod
     def load_llm_model():
-        print("Mistral model started downloading")
-        return CTransformers(model=LLM_MODEL_NAME, model_file=LLM_MODEL_FILE, model_type=LLM_MODEL_TYPE)
+        try:
+            print("Starting to download the Mistral model...")
+            llm_model = CTransformers(model=LLM_MODEL_NAME, model_file=LLM_MODEL_FILE, model_type=LLM_MODEL_TYPE)
+            print("Mistral model successfully loaded.")
+            return llm_model
+        except Exception as e:
+            print(f"Error loading the Mistral model: {e}")
+            return None
 
+    
     @staticmethod
     def load_text_file(file_path):
-        loader = TextLoader(file_path)
-        print("Transcript file loaded")
-        return loader.load()
+        try:
+            print(f"Loading transcript file from {file_path}...")
+            loader = TextLoader(file_path)
+            docs = loader.load()
+            print("Transcript file successfully loaded.")
+            return docs
+        except Exception as e:
+            print(f"Error loading text file: {e}")
+            return None
 
     @staticmethod
     def setup_database():
-        return lancedb.connect(DATABASE_PATH)
+        try:
+            print("Setting up the database...")
+            db = lancedb.connect(DATABASE_PATH)
+            print("Database setup complete.")
+            return db
+        except Exception as e:
+            print(f"Error setting up the database: {e}")
+            return None
 
     # embedding model
     @staticmethod
     def prepare_embeddings(model_name):
-        return HuggingFaceEmbeddings(model_name=model_name, model_kwargs={'device': 'cpu'})
+        try:
+            print(f"Preparing embeddings with model: {model_name}...")
+            embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs={'device': 'cpu'})
+            print("Embeddings prepared successfully.")
+            return embeddings
+        except Exception as e:
+            print(f"Error preparing embeddings: {e}")
+            return None
 
     @staticmethod
     def prepare_documents(docs):
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
-        return text_splitter.split_documents(docs)
+        if not docs:
+            print("No documents provided for preparation.")
+            return None
+        try:
+            print("Preparing documents...")
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+            documents = text_splitter.split_documents(docs)
+            print("Documents prepared successfully.")
+            return documents
+        except Exception as e:
+            print(f"Error preparing documents: {e}")
+            return None
+
 
     @staticmethod
     def run_query(query):
+        if not query:
+            print("No query provided.")
+            return "No query provided."
+        
+        print(f"Running query: {query}")
         docs = ChatWithVideo.load_text_file(TEXT_FILE_PATH)
+        if not docs:
+            return "Failed to load documents."
+        
         documents = ChatWithVideo.prepare_documents(docs)
+        if not documents:
+            return "Failed to prepare documents."
 
         embeddings = ChatWithVideo.prepare_embeddings(MODEL_NAME)
+        if not embeddings:
+            return "Failed to prepare embeddings."
+
         db = ChatWithVideo.setup_database()
+        if not db:
+            return "Failed to setup database."
 
-        table = db.create_table("pandas_docs", data=[
-            {"vector": embeddings.embed_query("Hello World"), "text": "Hello World", "id": "1"}
-        ], mode="overwrite")
-        docsearch = LanceDB.from_documents(documents, embeddings, connection=table)
+        try:
+            table = db.create_table("pandas_docs", data=[
+                {"vector": embeddings.embed_query("Hello World"), "text": "Hello World", "id": "1"}
+            ], mode="overwrite")
+            docsearch = LanceDB.from_documents(documents, embeddings, connection=table)
 
-        llm = ChatWithVideo.load_llm_model()
-        #llm =  ChatOpenAI(openai_api_key="sk-") use openai model directly
+            llm = ChatWithVideo.load_llm_model()
+            
+            if not llm:
+                return "Failed to load LLM model."
 
-
-        template = """Use the following pieces of context to answer the question at the end.
-        If you don't know the answer, just say that you don't know, don't try to make up an answer.
-        Use three sentences maximum and keep the answer as concise as possible.
-        Always say "thanks for asking!" at the end of the answer.
-        {context}
-        Question: {question}
-        Helpful Answer:"""
-
-        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
-        print("prompt loaded")
-        qa = RetrievalQA.from_chain_type(llm, chain_type='stuff', retriever=docsearch.as_retriever(), chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
-        return qa.run(query)
+            template = """Use the following pieces of context to answer the question at the end.
+            If you don't know the answer, just say that you don't know, don't try to make up an answer.
+            Use three sentences maximum and keep the answer as concise as possible.
+            Always say "thanks for asking!" at the end of the answer.
+            {context}
+            Question: {question}
+            Helpful Answer:"""
+    
+            QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+            print("prompt loaded")
+            qa = RetrievalQA.from_chain_type(llm, chain_type='stuff', retriever=docsearch.as_retriever(), chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+            print("Query processed successfully.")
+            return qa.run(query)
+        except Exception as e:
+            print(f"Error running query: {e}")
+            return f"Error: {e}"
