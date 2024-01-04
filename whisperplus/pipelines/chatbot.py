b09aa388e0d832deddd65468531d8d768b3e44dc
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2024-01-04 23:01:38 +0530

    Update chatbot.py

--- whisperplus/pipelines/chatbot.py
+++ whisperplus/pipelines/chatbot.py
@@ -136,3 +136,18 @@ class ChatWithVideo:
         except Exception as e:
             print(f"Error running query: {e}")
             return f"Error: {e}"
+
+
+
+
+
+
+# input_file = '/content/moe_blog.text'
+# llm_model_name = 'TheBloke/Mistral-7B-v0.1-GGUF'
+# llm_model_file = 'mistral-7b-v0.1.Q4_K_M.gguf'
+# llm_model_type = "mistral"
+# embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
+# chat = ChatWithVideo(input_file,llm_model_name, llm_model_file, llm_model_type, embedding_model_name)
+# query = "what is mistral ?"
+# response = chat.run_query(query)
+# print(response)
