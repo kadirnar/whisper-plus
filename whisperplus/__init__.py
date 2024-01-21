23421d5d06a27175031e49f3fdf6d191c193b61c
kadirnar <kadir.nar@hotmail.com>
2024-01-22 01:23:01 +0300

    🆙 Bump version to 0.2.7

--- whisperplus/__init__.py
+++ whisperplus/__init__.py
@@ -6,6 +6,6 @@ from whisperplus.pipelines.whisper_diarize import ASRDiarizationPipeline
 from whisperplus.utils.download_utils import download_and_convert_to_mp3
 from whisperplus.utils.text_utils import format_speech_to_dialogue
 
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 __author__ = 'kadirnar'
 __license__ = 'Apache License 2.0'
