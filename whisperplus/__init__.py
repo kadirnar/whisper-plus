300e4a21f76bb721f47209ec8416cca0bbb3c5e7
kadirnar <kadir.nar@hotmail.com>
2024-05-07 16:10:13 +0300

    🆙 Update version

--- whisperplus/__init__.py
+++ whisperplus/__init__.py
@@ -8,6 +8,6 @@ from whisperplus.pipelines.whisper import SpeechToTextPipeline
 from whisperplus.utils.download_utils import download_youtube_to_mp3, download_youtube_to_mp4
 from whisperplus.utils.text_utils import format_speech_to_dialogue
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __author__ = 'kadirnar'
 __license__ = 'Apache License 2.0'
